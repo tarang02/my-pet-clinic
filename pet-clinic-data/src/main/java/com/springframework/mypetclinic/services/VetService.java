60d10c60a4d1688ae52eea171562ab56bd01c3f9
TarangGupta <taranggupta@fico.com>
2022-09-13 17:06:29 +0530

    Implement interfaces for services

---
+++ pet-clinic-data/src/main/java/com/springframework/mypetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package com.springframework.mypetclinic.services;
+
+import com.springframework.mypetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
